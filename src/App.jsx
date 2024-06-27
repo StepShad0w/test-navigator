0f4bb527c62b0293e6df861aec7a9ebf9a92cf45
StepShad0w <boykkostepan@egmai.com>
2024-06-27 11:25:23 +0300

    t

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { openDeviceVPNSettings } from './Vpn';
+// import openDeviceVPNSettings from "./Vpn"
 
 function App() {
   const [count, setCount] = useState(0);
