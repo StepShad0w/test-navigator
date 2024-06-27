3642836267569f11e1f505eabbf3daf44c7bbcb2
StepShad0w <boykkostepan@egmai.com>
2024-06-27 11:51:59 +0300

    f

--- src/Vpn.tsx
+++ src/Vpn.tsx
@@ -1,26 +1,26 @@
 export const openDeviceVPNSettings = () => {
     const ua = navigator.userAgent;
-  
+
     if (/android/i.test(ua)) {
-      // Відкрити налаштування VPN на Android
-      window.location.href = 'intent:#Intent;action=android.settings.VPN_SETTINGS;end';
+        // Відкрити налаштування VPN на Android
+        window.location.href = 'intent:#Intent;action=android.settings.VPN_SETTINGS;end';
     } else if (/iPad|iPhone|iPod/.test(ua) && !(window as any).MSStream) {
-      // Відкрити загальні налаштування на iOS
-      window.location.href = 'app-settings:';
-      alert('Будь ласка, перейдіть у Налаштування > Загальні > VPN, щоб налаштувати VPN.');
+        // Відкрити налаштування VPN на iOS
+        window.location.href = 'app-settings:';
+        alert('Будь ласка, перейдіть у Налаштування > Загальні > VPN, щоб налаштувати VPN.');
     } else if (/Windows Phone/i.test(ua)) {
-      // Відкрити налаштування VPN на Windows Phone
-      window.location.href = 'ms-settings:network-vpn';
+        // Відкрити налаштування VPN на Windows Phone
+        window.location.href = 'ms-settings:network-vpn';
     } else if (/Windows/i.test(ua)) {
-      // Відкрити налаштування VPN на Windows
-      window.location.href = 'ms-settings:network-vpn';
+        // Відкрити налаштування VPN на Windows
+        window.location.href = 'ms-settings:network-vpn';
     } else if (/Macintosh|MacIntel|MacPPC|Mac68K/.test(ua)) {
-      // Відкрити налаштування мережі на macOS, найближче до VPN
-      window.location.href = 'x-apple.systempreferences:com.apple.preference.network';
-    } else if (/Linux|armv8l/i.test(ua)) {
-      // Для Linux, надати інструкції
-      alert('Будь ласка, відкрийте налаштування VPN вручну у вашій системі.');
+        // Відкрити налаштування мережі на macOS, найближче до VPN
+        window.location.href = 'x-apple.systempreferences:com.apple.preference.network';
+    } else if (/Linux/i.test(ua) && /arm/i.test(ua)) {
+        // Відкрити налаштування VPN на Linux з ARM архітектурою
+        alert('Будь ласка, відкрийте налаштування VPN вручну у вашій системі.');
     } else {
-      alert('Налаштування не можуть бути відкриті на цьому пристрої.');
+        alert('Налаштування не можуть бути відкриті на цьому пристрої.');
     }
-  };
+};
